4b187ac78e6b1da9f4446086616f39c8e91ff4ed
dayas-kr <149292309+dayas-kr@users.noreply.github.com>
2025-11-15 11:19:18 +0530

    feat(ui): Add lockScroll magic helper to Alpine.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,4 +4,22 @@ import focus from "@alpinejs/focus";
 
 Alpine.plugin(focus);
 window.Alpine = Alpine;
+
+// Add the lockScroll magic helper
+Alpine.magic("lockScroll", () => {
+    return (isOpen, transitionDuration = 150) => {
+        if (isOpen) {
+            const scrollbarWidth =
+                window.innerWidth - document.documentElement.clientWidth;
+            document.body.style.paddingRight = scrollbarWidth + "px";
+            document.body.style.overflow = "hidden";
+        } else {
+            setTimeout(() => {
+                document.body.style.paddingRight = "";
+                document.body.style.overflow = "";
+            }, transitionDuration);
+        }
+    };
+});
+
 Alpine.start();
