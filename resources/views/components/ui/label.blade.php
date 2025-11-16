76b66e0d84add47342d283dcd54f23278d32b623
dayas-kr <149292309+dayas-kr@users.noreply.github.com>
2025-11-16 14:36:46 +0530

    feat(ui): Refactor Label component for improved class management

--- resources/views/components/ui/label.blade.php
+++ resources/views/components/ui/label.blade.php
@@ -1,16 +1,4 @@
-@props(['for' => null])
-
-@php
-    $classes = implode(' ', [
-        'relative w-fit',
-        'flex items-center gap-2',
-        'text-sm leading-none font-medium select-none',
-        'data-[disabled=true]:opacity-50',
-        '[&[data-required=true]_[data-slot=label-required-asterisk]]:block',
-    ]);
-@endphp
-
-<label data-slot="label" {{ $attributes->merge(['for' => $for, 'class' => $classes]) }}>
+<label data-slot="label"
+    {{ $attributes->merge(['class' => 'flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50']) }}>
     {{ $slot }}
-    <span data-slot="label-required-asterisk" class="text-red-400 absolute -top-0.5 -right-2 hidden">*</span>
 </label>
